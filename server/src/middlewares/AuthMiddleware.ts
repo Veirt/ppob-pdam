8c809b51af3fc30602f2e187bc3cdf82bb9a4486
Veirt <vveirt@gmail.com>
2022-02-14 23:05:45 +0800

    feat: add authorizatio middlewares

--- server/src/middlewares/AuthMiddleware.ts
+++ server/src/middlewares/AuthMiddleware.ts
@@ -4,6 +4,7 @@ export const isAuthenticated: Middleware = (req, res, next) => {
     if (!req.isAuthenticated()) {
         return res.status(401).json({ msg: "Not authenticated" });
     }
+
     return next();
 };
 
@@ -11,6 +12,26 @@ export const isAdmin: Middleware = (req, res, next) => {
     if (!req.user) return res.status(401).json({ msg: "Not authenticated" });
 
     if (req.user.role.id_role !== 3) {
+        return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    return next();
+};
+
+export const isPetugasMeteran: Middleware = (req, res, next) => {
+    if (!req.user) return res.status(401).json({ msg: "Not authenticated" });
+
+    if (req.user.role.id_role !== 2) {
+        return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    return next();
+};
+
+export const isPetugasLoket: Middleware = (req, res, next) => {
+    if (!req.user) return res.status(401).json({ msg: "Not authenticated" });
+
+    if (req.user.role.id_role !== 1) {
         return res.status(401).json({ msg: "Not authorized" });
     }
 
